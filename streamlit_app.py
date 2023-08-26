a2070a54772c6b5cbac3f7e6a80ad540686a3a9c
Chirag Saraogi <chiragsaraogi15@gmail.com>
2023-08-26 10:03:07 -0400

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,7 @@ import tensorflow as tf
 import numpy as np
 from tensorflow.keras.models import load_model
 from mtcnn import MTCNN
+from PIL import Image
 
 # Load the trained model
 model = load_model('model.h5')  # Replace with your model path
